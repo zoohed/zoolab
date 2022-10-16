c6803f675b771133019cd57e96704ddc70f2ac1c
zoohed <115933168+zoohed@users.noreply.github.com>
2022-10-17 01:13:30 +0900

    Add files via upload

---
+++ 56p programming 06.py
@@ -0,0 +1,9 @@
+#56p Programming
+
+#06
+import turtle as t
+t.shape("turtle")
+t.forward(300)
+t.left(90)
+t.forward(200)
+t.done()
---
+++ 56p programming 08.py
@@ -0,0 +1,8 @@
+#56p Programming
+
+#08
+import turtle as t
+t.shape("turtle")
+t.color("blue")
+t.forward(100)
+t.done()
