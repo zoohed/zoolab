dd8bfd1b490145cf7d55e43c9eabc96312390382
zoohed <115933168+zoohed@users.noreply.github.com>
2022-11-28 01:20:55 +0900

    Add files via upload

---
+++ 01 first study..py
@@ -0,0 +1,140 @@
+#31p 중간점검
+
+#01
+print("안녕하세요?")
+
+
+#02
+print("programming에 입문하신 것을 축하드립니다.")
+
+
+#05
+print("생일축하!!" * 10)
+
+
+#06
+print("Hello") #뒤에 " 빠짐
+
+
+
+#38p 중간점검
+
+#01
+print("3 * 1 =", 3*1)
+
+
+#02
+print("3 * 2 =", 3*2)
+
+
+#03
+print("3 * 3 =", 3*3)
+
+
+#39p 도전문제
+
+print("*"*38)
+print("print()를 사용한 예제입니다.")
+print("내가 제일 좋아하는 숫자는 7입니다.")
+print("*"*38)
+
+
+#49p 중간점검
+
+#01
+import turtle as t 
+t.shape("turtle")
+for i in range(3): 
+    t.forward(200)
+    t.left(120)     #360(도)/3(각형)
+t.done()
+
+
+#02
+import turtle as t
+t.shape("turtle")
+for i in range(4):
+    t.forward(200)
+    t.left(90)   #360(도)/4(각형)
+t.done()
+
+
+#03
+import turtle as t
+t.shape("turtle")
+for i in range(6):
+    t.forward(200)
+    t.left(60)  #360(도)/6(각형)
+t.done()
+
+
+#55p Programming
+
+#01
+print("환영합니다.")
+print("파이썬의 세계로 오신 것을 환영합니다.")
+print("파이썬은 강력합니다.")
+
+
+#02
+print("+"+"="*16+"+")
+print(":"+" "*13+"홍길동"+" "*13+":")
+print("+"+"="*16+"+")
+
+
+#03
+print(1 + 2 + 3)
+
+
+#04
+print("1+2+3을 계산하면 %d입니다." %(1+2+3))
+
+
+#05
+import turtle as t
+t.shape("turtle")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.done()
+
+
+#06
+import turtle as t
+t.shape("turtle")
+t.forward(300)
+t.left(90)
+t.forward(200)
+t.done()
+
+
+#07
+import turtle as t
+t.shape("turtle")
+t.width(10)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.done()
+
+
+#08
+import turtle as t
+t.shape("turtle")
+t.color("blue")
+t.forward(100)
+t.done()
+
+
+#09
+import turtle as t
+t.shape("square")
+t.forward(100)
+t.done()
+
